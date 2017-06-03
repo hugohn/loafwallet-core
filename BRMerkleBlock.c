8a6f4c320c0fdf7242e1ad2245e46ea00b16ddba
Hugo Nguyen <hugo@scribd.com>
2017-06-02 23:53:49 -0700

    add debug messages

--- BRMerkleBlock.c
+++ BRMerkleBlock.c
@@ -267,20 +267,32 @@ int BRMerkleBlockIsValid(const BRMerkleBlock *block, uint32_t currentTime)
     int r = 1;
     
     // check if merkle root is correct
-    if (block->totalTx > 0 && ! UInt256Eq(merkleRoot, block->merkleRoot)) r = 0;
+    if (block->totalTx > 0 && ! UInt256Eq(merkleRoot, block->merkleRoot)) {
+        sprintf("invalid merkleRoot: %s - %s", u256_hex_encode(merkleRoot), u256_hex_encode(block->merkleRoot));
+        r = 0;
+    }
     
     // check if timestamp is too far in future
-    if (block->timestamp > currentTime + BLOCK_MAX_TIME_DRIFT) r = 0;
+    if (block->timestamp > currentTime + BLOCK_MAX_TIME_DRIFT) {
+        sprintf("timestamp too far in future: %d - %d", block->timestamp, (currentTime + BLOCK_MAX_TIME_DRIFT));
+        r = 0;
+    }
     
     // check if proof-of-work target is out of range
-    if (target == 0 || target & 0x00800000 || size > maxsize || (size == maxsize && target > maxtarget)) r = 0;
+    if (target == 0 || target & 0x00800000 || size > maxsize || (size == maxsize && target > maxtarget)) {
+        sprintf("target is out of range: %x - %x - %x - %x", target, maxtarget, size, maxsize);
+        r = 0;
+    }
     
     if (size > 3) UInt32SetLE(&t.u8[size - 3], target);
     else UInt32SetLE(t.u8, target >> (3 - size)*8);
     
     for (int i = sizeof(t) - 1; r && i >= 0; i--) { // check proof-of-work
         if (block->powHash.u8[i] < t.u8[i]) break;
-        if (block->powHash.u8[i] > t.u8[i]) r = 0;
+        if (block->powHash.u8[i] > t.u8[i]) {
+            sprintf("invalid pow [%d]: %x - %x", i, block->powHash.u8[i], t.u8[i]);
+            r = 0;
+        }
     }
     
     return r;
